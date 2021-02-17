eaa48c36fc5ef0b8247c1e74b3b98a516853be57
Max Kapur <maxkapur@gmail.com>
2021-02-17 11:44:43 +0900

    quality and cutoff operators

--- examples/SchoolIncentives.jl
+++ examples/SchoolIncentives.jl
@@ -3,6 +3,28 @@
 
 using DeferredAcceptance
 
+# Might add these functions to the module later.
+function quality(assn, capacities, scores)
+	(m, ) = size(capacities)
+	res = zeros(m)
+	for (s, c) in enumerate(assn)
+		if c ≤ m
+			res[c] += scores[s, c]
+		end
+	end
+	res ./= capacities
+	return res
+end
+
+function cutoffs(assn, capacities, scores)
+	(m, ) = size(capacities)
+	res = zeros(m)
+	for c in 1:m
+		res[c] = minimum(scores[:, c][assn .== c])
+	end
+	return res
+end
+
 students = [2 1 1 1 1;
 			1 2 2 2 2]
 
@@ -21,16 +43,27 @@ schools = mapslices(argsort, 1 .- scores, dims=2)
 
 capacities = [2, 3]
 
-println("Normal: ",DA(students, schools', capacities)[1])
-println("Delta:  ",DA(students_delta, schools', capacities)[1])
-# Normal: [2, 1, 1, 2, 2]
-# Delta:  [2, 2, 1, 1, 2]
+out_normal = DA(students, schools', capacities)
+out_delta = DA(students_delta, schools', capacities)
+
+println("Normal assn:  ", out_normal[1])
+println("    Quality:  ", quality(out_normal[1], capacities, scores'))
+println("    Cutoffs:  ", cutoffs(out_normal[1], capacities, scores'))
+println("Delta:        ", out_delta[1])
+println("    Quality:  ", quality(out_delta[1], capacities, scores'))
+println("    Cutoffs:  ", cutoffs(out_delta[1], capacities, scores'))
+# Normal assn:  [2, 1, 1, 2, 2]
+#     Quality:  [0.925, 0.8500000000000001]
+#     Cutoffs:  [0.89, 0.68]
+# Delta:        [2, 2, 1, 1, 2]
+#     Quality:  [0.86, 0.8300000000000001]
+#     Cutoffs:  [0.83, 0.68]
 
 #=  School 2 ends up losing a great student (4), because persuading student 2
 	away from school 1 meant that school 1 lowered its cutoff and 4 got in. =#
 
 # Shows that the stable match is unique.
-println("Normal-rev: ",DA(students, schools', capacities, rev=true)[1])
-println("Delta-rev:  ",DA(students_delta, schools', capacities, rev=true)[1])
-# Normal: [2, 1, 1, 2, 2]
-# Delta:  [2, 2, 1, 1, 2]
+println("Normal-rev:   ",DA(students, schools', capacities, rev=true)[1])
+println("Delta-rev:    ",DA(students_delta, schools', capacities, rev=true)[1])
+# Normal-rev:   [2, 1, 1, 2, 2]
+# Delta-rev:    [2, 2, 1, 1, 2]
